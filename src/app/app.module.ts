796fc5ac2f91f166fc5c48e19670368e52260861
Sharon Kimani <sharonkimani01@gmail.com>
2020-08-27 23:35:22 +0300

    Created a search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RepoComponent } from './repo/repo.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    RepoComponent
+    RepoComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
