e7bed0a7c530eafbe3e955a946ec443e9d5558a6
Sharon Kimani <sharonkimani01@gmail.com>
2020-08-28 00:37:00 +0300

    Added logic to user ts file

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,3 +1,6 @@
+import { ServiceService } from './../service/service.service';
+import { Repo } from './../repo';
+import { User } from './../user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  repo: Repo;
 
-  ngOnInit(): void {
+  constructor(public userService: ServiceService, private repoService: ServiceService) { }
+
+  search(search) {
+    this.userService.getUser(search).then(
+      success => {
+        this.user = this.userService.users;
+      },
+      error => {
+        console.log(error)
+      }
+    );
+
+    this.repoService.getRepos(search).then(
+      result => {
+        this.repo = this.repoService.repos;
+        console.log(this.repo);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  ngOnInit() {
+    this.search('greatdaniels');
   }
 
 }
