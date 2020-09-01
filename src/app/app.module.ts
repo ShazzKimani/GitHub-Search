bfa5eeab2a44e4f2550e6f0f2f2b26276445ffcc
Sharon Kimani <sharonkimani01@gmail.com>
2020-09-01 16:28:25 +0300

    Removed Loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgProgressModule } from '@ngx-progressbar/core';
-import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,9 +27,8 @@ import { StrikethroughDirective } from './strikethrough.directive';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    NgProgressModule.forRoot(),
-    NgProgressHttpClientModule
+    HttpClientModule
+    
   ],
   providers: [ServiceService],
   bootstrap: [AppComponent]
